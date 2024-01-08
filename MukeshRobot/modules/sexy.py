d8c3c85ed78bf25f1086029240818a101435b141
SOUL X SYSTEM <154720925+mahtoanjali@users.noreply.github.com>
2024-01-08 13:45:18 +0530

    Update sexy.py

--- MukeshRobot/modules/sexy.py
+++ MukeshRobot/modules/sexy.py
@@ -4,7 +4,7 @@ from telethon import Button, events
 
 from .. import telethn as asst
 
-BUTTON = [[Button.url("🍒 ꜱᴜᴘᴘᴏʀᴛ 🍒", "https://t.me/TG_FRIENDSS")]]
+BUTTON = [[Button.url("🍒 ꜱᴜᴘᴘᴏʀᴛ 🍒", "https://t.me/AnjalixSupportxGroup")]]
 HOT = "https://telegra.ph/file/daad931db960ea40c0fca.gif"
 SMEXY = "https://telegra.ph/file/a23e9fd851fb6bc771686.gif"
 LEZBIAN = "https://telegra.ph/file/5609b87f0bd461fc36acb.gif"
@@ -84,7 +84,7 @@ __help__ = """
 
 ➻ /cute - ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ᴄᴜᴛᴇɴᴇꜱꜱ
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𓆩 ᴍᴧʜᴛᴏ ✗ ᴧɴᴊᴀʟɪ 𓆪](https://t.me/QUEENx_GOD)............☆
 """
 
 __mod_name__ = "⚡Sᴇᴍxʏ⚡"
